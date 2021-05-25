cc93ff636b0335d920fa78b01fd03a6e73bd0d7f
Siddhant <siddhant1042@gmail.com>
2021-05-25 20:11:05 +0530

    Added Cowin Check

--- src/availability/lambda_availability.py
+++ src/availability/lambda_availability.py
@@ -14,27 +14,40 @@ import datetime
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-cowin_url = os.getenv['appointment']
-chat = os.getenv['chat']
 
 def availablity():
+    cowin_url = os.environ['appointment']
     now = datetime.datetime.now() 
     now = now.strftime("%d-%m-%Y")    
     try:
-        logger.info(f"Requesting {cowin_url}")
-        res = requests.get(cowin_url+'&date='+now)
-        logger.info(f"Request successful")
+        final_url = cowin_url+'&date='+now
+        logger.info(f"Requesting {final_url}")
+        headers = {'user-agent': 'my-app/0.0.1'}
+        res = requests.get(final_url,headers=headers)
+
+        logger.info(f"Request successful {res}")
         json_response = res.json()
         reply = check(json_response)
         logger.info(f"reply constructed as {reply}")
-        send_info(reply)
+
+        if reply["available"]:  
+            send_info(reply["slots"])
+        else:
+            send_info("Nothing Available")
     except RequestException as e:
         logger.error(f"Cowin API Call Failed {e}")
+        try:
+            send_info("Call API Failed")
+        except Exception as f:
+            logger.error(f"Tragic Error {f}")
     
 
 def check(response):
     centers = response['centers']
-    reply = []
+    reply = {
+        "available" : False
+    }
+    available = []
     for i in centers:
         name = i['name']
         sessions = i['sessions']
@@ -45,22 +58,28 @@ def check(response):
             date = s['date']
             if min_age < 45 and dose_1 > 0:
                 x = f"{dose_1} does of {vaccine} is available at {name} on {date} "
-                reply.append(x)
+                available.append(x)
 
-    if len(reply>0):
-        reply = "\n".join(reply)
-    else:
-        reply = "No Slots Are Available"
+    if len(available) > 0:
+       reply["available"] = True
+       reply["slots"] = "\n".join(available)
+
     return reply
 
 def send_info(reply):
+    chat = os.environ['chat']
     data = {}
     data['message']=reply
     data['chat_id']=chat
     data = json.dumps(data)
+    lambda_client = boto3.client('lambda')
     response = lambda_client.invoke(
-        FunctionName="sendAppointmentDetails",
+        FunctionName="bank-communication",
         InvocationType='Event',
         Payload=data
     )
-    logger.info(f"Invoked sendAppointmentDetails lambda with response f{response}")
+    logger.info(f"Invoked bank-communication lambda with response f{response}")
+
+
+def lambda_handler(event, context):
+    availablity()
